f48ff6d7c27134c696c662d9c5bf7370e90e74d9
ricklai <ricklai82@gmail.com>
2020-04-24 13:55:43 +0800

    下載報名表件

--- src/js/result.js
+++ src/js/result.js
@@ -46,19 +46,19 @@
 			}
 		})
 		.then((json) => {
-			if(!json.has_admission) {
-				alert('請先選擇分發志願類組');
-				location.href = './admission.html';
-			} else if(!json.has_apply_way) {
-				alert('請先選擇成績採計方式');
-				location.href = './grade.html';
+			if(!json.has_qualify) {
+				alert('請先完成資格檢視');
+				location.href = './qualify.html';
 			} else if(!json.has_personal_info) {
 				alert('請先填寫個人基本資料');
 				location.href = './personalInfo.html';
-			}  else if(!json.has_qualify) {
-				alert('請先完成資格檢視');
-				location.href = './qualify.html';
-			}  else{
+			} else if(!json.has_apply_way) {
+				alert('請先選擇成績採計方式');
+				location.href = './grade.html';
+			} else if(!json.has_admission) {
+				alert('請先選擇分發志願類組');
+				location.href = './admission.html';
+			} else{
 				$previewPersonalDataBtn.attr('href', env.baseUrl + '/malaysia-spring/admission-paper/department-apply-form');
 			}
 			loading.complete();
