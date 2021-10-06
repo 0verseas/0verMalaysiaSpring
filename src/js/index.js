13347243ed532682319f69959648cc5e17912247
Rick <ricklai82@gmail.com>
2021-10-06 13:21:27 +0800

    問卷與截止日更新

--- src/js/index.js
+++ src/js/index.js
@@ -32,14 +32,9 @@
 	async function _init() {
 		try {
             $downloadLinks.append(
-            	// '<a href="' + env.baseUrl + '/forms/2019志願選填說明書(香港DSE、CEE、ALE學生適用)_2.pdf" target="_blank" class="list-group-item list-group-item-action">2019志願選填說明書(香港DSE、CEE、ALE學生適用)</a>' +
-				// '<a href="' + env.baseUrl + '/forms/香港各會考文憑成績核計方式及分數換算對照表.pdf" target="_blank" class="list-group-item list-group-item-action">香港各會考文憑成績核計方式及分數換算對照表</a>\n' +
-                // '<a href="' + env.baseUrl + '/hk-quit-admission-placement-declaration-form" target="_blank" class="list-group-item list-group-item-action">放棄聯合分發聲明書（香港專用）</a>\n' +
                 '<a href="http://get.adobe.com/tw/reader/" target="_blank" class="list-group-item list-group-item-action">觀看中文PDF軟體</a>\n' +
                 '<a href="http://www.7-zip.org/" target="_blank" class="list-group-item list-group-item-action">解壓縮軟體</a>\n' +
-                '<a href="' + env.baseUrl + '/admission-data-correction-form" target="_blank" class="list-group-item list-group-item-action">報名資料修正表</a>\n' +
-                '<a href="http://www.gov.hk/tc/about/helpdesk/softwarerequirement/hkscs.htm" target="_blank" class="list-group-item list-group-item-action">香港字庫(For win2000/XP OR Linux)</a>\n' +
-                '<a href="http://www.ccli.gov.hk/tc_chi/faq/faq_win7-hkscs.html" target="_blank" class="list-group-item list-group-item-action">香港字庫(FOR Win7)</a>'
+                '<a href="' + env.baseUrl + '/admission-data-correction-form" target="_blank" class="list-group-item list-group-item-action">報名資料修正表</a>'
 			);
 			const response = await student.getAdmissionCount();
 			if (!response.ok) { throw response; }
