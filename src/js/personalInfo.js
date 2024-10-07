4abee66e2fcbf0b0d9c84f5666945783cbc9fe4b
ricklai82 <ricklai82@gmail.com>
2024-10-07 09:44:28 +0800

    修改生日預設選項與上下限

--- src/js/personalInfo.js
+++ src/js/personalInfo.js
@@ -213,34 +213,15 @@
 
                 // 總是有人亂填生日 甚至變成未來人 只好設個上限 最年輕就是報名當下剛滿十歲
                 let Year = new Date().getFullYear();
-                let birthDayDefault = `defaultViewDate: {year: (Year-18)}`;
-                if(formData.birthday != null){
-                    birthDayDefault = `defaultDate: ${formData.birthday}`;
-                }
-                let dadBirthDayDefault = `defaultViewDate: {year: (Year-40)}`;
-                if(formData.dad_birthday != null){
-                    dadBirthDayDefault = `defaultDate: ${formData.dad_birthday}`;
-                }
-                let momBirthDayDefault = `defaultViewDate: {year: (Year-40)}`;
-                if(formData.mom_birthday != null){
-                    momBirthDayDefault = `defaultDate: ${formData.mom_birthday}`;
-                }
-                let schoolAdmissionAtDefault = `defaultViewDate: {year: (Year-5)}`;
-                if(formData.school_admission_at != null){
-                    schoolAdmissionAtDefault = `defaultDate: ${formData.school_admission_at}`;
-                }
-                let schoolGraduateAtDefault = `defaultViewDate: {year: (Year)}`;
-                if(formData.school_graduate_at != null){
-                    schoolGraduateAtDefault = `defaultDate: ${formData.school_graduate_at}`;
-                }
                 $birthday.datepicker({
                     updateViewDate: true, // 會自動避免並修正直接輸入錯誤/無效的月/日，例：不是潤年的時候輸入2月29日，設true會自動跳下一天到3月1日
                     autoclose: true, // 選完會自動關閉選擇器
                     startView: 2, // 以個位數年份單位開始瀏覽
                     maxViewMode: 3, // 最高以10年單位瀏覽年份
                     immediateUpdates: true, // 只要選了其中一個項目，立即刷新欄位的年/月/日的數字
-                    birthDayDefault,
-                    endDate: new Date(new Date().setFullYear(new Date().getFullYear() - 10))
+                    defaultViewDate: {year: (Year-18)}, // 預設選項是 18歲
+                    startDate: '-121y', // 當前年份-121y
+                    endDate: '-9y' // 當前年份-9y
                 });
                 // 總是有人亂填生日 甚至變成未來人 只好設個上限 父母最年輕就是報名當下剛滿二十二歲
                 $dadBirthday.datepicker({
@@ -250,7 +231,8 @@
                     maxViewMode: 3,
                     immediateUpdates: true,
                     defaultViewDate: {year: (Year-40)},
-                    endDate: new Date(new Date().setFullYear(new Date().getFullYear() - 22))
+                    startDate: '-121y',
+                    endDate: '-21y'
                 });
                 $momBirthday.datepicker({
                     updateViewDate: true,
@@ -259,7 +241,8 @@
                     maxViewMode: 3,
                     immediateUpdates: true,
                     defaultViewDate: {year: (Year-40)},
-                    endDate: new Date(new Date().setFullYear(new Date().getFullYear() - 22))
+                    startDate: '-121y',
+                    endDate: '-21y'
                 });
                 $schoolAdmissionAt.datepicker({
                     updateViewDate: true,
